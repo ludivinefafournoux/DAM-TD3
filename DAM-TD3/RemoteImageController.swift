f9b13d0f1abc16bd50383e5beab53c43f79a7b03
Laura Daufeld <laura.daufeld@gmail.com>
2017-01-20 22:30:44 +0100

    Nettoyage code

--- DAM-TD3/RemoteImageController.swift
+++ DAM-TD3/RemoteImageController.swift
@@ -17,8 +17,6 @@ class RemoteImageController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        
         // imageview
         imageView1 = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
         
@@ -69,46 +67,11 @@ class RemoteImageController: UIViewController {
         imageView1.image = UIImage(data: data!)
         imageView1.contentMode = .scaleAspectFit
     }
-    
-    
-    /*func buttonAction1(sender: UIButton!) {
-        print("Button1 tapped")
-        self.view.addSubview(self.imageView1)
-        self.view.sendSubview(toBack: self.imageView1)
-    }
-    
-    func buttonAction2(sender: UIButton!) {
-        print("Button2 tapped")
-        imageView1.backgroundColor = UIColor.black
-        self.imageView1.image = UIImage(named: "pandaroux")
-        imageView1.contentMode = .scaleAspectFit
-    }
-    
-    func buttonAction3(sender: UIButton!) {
-        print("Button3 tapped")
-        imageView1.backgroundColor = UIColor.black
-        let url = URL(string: "http://www.clipartkid.com/images/293/re-uploading-old-art-red-panda-by-sarilain-0JgNA8-clipart.png")
-        let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
-        imageView1.image = UIImage(data: data!)
-        imageView1.contentMode = .scaleAspectFit
-        
-    }*/
 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
